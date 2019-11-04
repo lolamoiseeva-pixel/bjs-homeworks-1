bb9cde8963c852f867ff1af7bc17bab8bf996a04
Lola Moiseeva <lola.moiseeva@gmail.com>
2019-11-04 17:21:11 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,21 @@
+console.log("Task 1");
+console.log();
+let algebra = 5;
+let geography = 4;
+let physics = 3;
+let average = (algebra + geography + physics) / 3;
+console.log(average);
+
+console.log("Task 2");
+console.log();
+let myName = "Lola";
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+
+console.log("task 3");
+console.log();
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
