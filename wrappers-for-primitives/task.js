f5de1f6ce5e948ae9c1cd08a47ec9a29f0a6944b
Lola Moiseeva <lola.moiseeva@gmail.com>
2019-11-14 14:20:11 +0300

    my commit

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -13,7 +13,70 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
     // код для задачи №1 писать здесь
     //return totalAmount;
+
+    'use strict';
+
+    function checkInputData (input) {
+
+        let value;
+
+        if (typeof(input) === 'number') {
+            value = input;
+            return value;
+        
+        } else  if (typeof (input) === 'string'){
+            
+            value  = parseInt(input);
+        }
+          if (typeof(value) === 'Number'&& ! isNaN(value)){
+            return value;
+          }    
+         else {
+          console.log(`Параметр ${input} содержит неправильное значение ${value}`);
+        }
+        
+    }
+    
+
+    function getMonthsOfMorgage(date) {
+      
+      let date2 = new Date();
+
+      let date1 = new Date(date);
+      
+      let year2 = date2.getFullYear();
+      
+      let year = date1.getFullYear();
+
+      let months;
+
+      months = (year - year2) * 12
+
+      months += date1.getMonth() - date2.getMonth();
+
+  
+      return months;
+    }
+
+
+    let monthsToPay = getMonthsOfMorgage(date);
+    
+
+    let returnValue = checkInputData(amount) - checkInputData(contribution);
+
+    let p = checkInputData(percent) / 1200;
+
+    let c = Math.pow((1 + p), monthsToPay);
+
+    let monthlyPayment = returnValue * (p + (p / (c - 1)));
+
+    let totalAmount = returnValue + monthlyPayment * monthsToPay;
+
+    return totalAmount;
+
 }
+
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -25,4 +88,18 @@ function sayHello() {
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     //return greeting;
+
+    let greeting;
+
+
+    if (typeof(name)=== 'string' && name !== '') {
+    greeting = `Привет, мир! Меня зовут ${name}`;
+    
+    } else {
+    
+        greeting = 'Привет, мир! Меня зовут Аноним';
+    
+    }
+ 
+  return greeting;
 }
